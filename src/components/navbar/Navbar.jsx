aa1ac17f015c2e14eea7ebfb720988b8da47ed98
Haythem <sinnner6@gmail.com>
2023-01-20 13:37:07 +0100

    implemented callToAction component and reorderd nav links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,10 +12,10 @@ const Menu = () => (
       <a href="#wgpt3">What is GPT3?</a>
     </p>
     <p>
-      <a href="#possibility">Open AI</a>
+      <a href="#features">Case Studies</a>
     </p>
     <p>
-      <a href="#features">Case Studies</a>
+      <a href="#possibility">Open AI</a>
     </p>
     <p>
       <a href="#blog">Library</a>
